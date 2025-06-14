398ed61f173aff92d501ff0058f4f81b66d86007
kartik855-on <kartik.jaat.90799@gmail.com>
2025-06-14 10:12:26 +0530

    Add files via upload

---
+++ 3_Strings/Operators.py
@@ -0,0 +1,34 @@
+# Arithmetic Operators
+print("Arithmetic Operators")
+a = 10
+b = 5
+print(a + b)  
+print(a - b) 
+print(a * b)  
+print(a / b)  
+print(a // b) #  (floor division)
+print(a % b)  #  (modulus)
+print(a ** b) #  (power)
+
+
+#Assignment Operators
+x = 5
+x += 2  # x = x + 2 = 7
+x *= 3  # x = x * 3 = 21
+
+# Comparison Operators/relational operators
+print("Comparison Operators")
+a = 10
+b = 20
+print(a == b)  # False
+print(a != b)  # True
+print(a < b)   # True
+print(a >= b)  # False
+
+#Logical Operators
+print("Logical Operators")
+x = 5
+print(x > 2 and x < 10)  # True ( cantion1 and cantion2 dono right ho )
+print(x > 2 or x > 10)   # True (any cantion me 1 right ho)
+print(not(x > 2))        # False
+
---
+++ 3_Strings/datatype.py
@@ -0,0 +1,8 @@
+a = 10	     # Int
+b = 3.14     # float
+c = "Hello"  # String 
+d = True	 # Boolean 
+e = None	 # No value 
+
+#Type Checking format
+print(type(a))
