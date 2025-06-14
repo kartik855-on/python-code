ff97ca5c4c4bde9c59211b80e6d5501103433bec
kartik855-on <kartik.jaat.90799@gmail.com>
2025-06-14 10:55:47 +0530

    Create 1_class.py

---
+++ 10_OOPs/1_class.py
@@ -0,0 +1,12 @@
+class Employee: 
+    language = "Py" # class attribute
+    salary = 1200000
+
+
+kartik = Employee()
+kartik.name = "kartik" # instance attribute 
+print(kartik.name, kartik.language, kartik.salary)
+
+rohan = Employee()
+rohan.name = "Rohan Roro Robinson"
+print(rohan.name, rohan.salary, rohan.language)
