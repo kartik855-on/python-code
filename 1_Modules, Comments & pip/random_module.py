35a2236c5396ff0e03cf824eef04b3ce5bcc8186
kartik855-on <kartik.jaat.90799@gmail.com>
2025-06-14 09:59:49 +0530

    Create random_module.py

---
+++ 1_Modules, Comments & pip/random_module.py
@@ -0,0 +1,10 @@
+import random
+print(random.randint(1,10))
+print(random.choice(["a","b","c"]))
+
+# use frome to import 
+from random import randint
+print(randint(1,10))
+
+from random import choice
+print(choice(["kartik","amit","nitesh"]))
