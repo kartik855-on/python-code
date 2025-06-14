08529fadac195230daea892a198e88b3dd26f543
kartik855-on <kartik.jaat.90799@gmail.com>
2025-06-14 10:07:14 +0530

    Create 1_Variables.py

---
+++ 2_Variables and Datatype/1_Variables.py
@@ -0,0 +1,22 @@
+x = 5           # Integer
+name = "Kartik" # String
+pi = 3.14       # Float
+is_active = True # Boolean
+
+print(type(x))
+print(type(name))
+print(type(pi))
+print(type(is_active))
+
+
+# Valid Variable Names
+my_var = 10
+_myvar = 20
+var1 = 30
+
+# Invalid Variable Names
+'''
+1var = 10       #  start with number
+my-var = 20     #  - not allowed
+class = 30      #  class is keyword
+'''
